d703c20f30602430447d87fa57328c3897ff4e0e
RDYES <r.done@yesconnected.org>
2026-05-15 14:20:26 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA4mqTVMfzKeyJjZfstcvRVyE3oa9_CO0Y",
   authDomain: "yesconnectedpshe.firebaseapp.com",
   projectId: "yesconnectedpshe",
-  storageBucket: "yesconnectedpshe.appspot.com",
+  storageBucket: "yesconnectedpshe.firebasestorage.app",
   messagingSenderId: "1018281902876",
   appId: "1:1018281902876:web:f28e9d77c8a5f55aeb30d9"
 };
